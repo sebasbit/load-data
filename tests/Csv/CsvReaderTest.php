73d09ecbd547d31611e28b5120384af63291faa3
sebasbit <sebastian.torres0426@gmail.com>
2022-06-09 22:28:52 -0500

    First commit

---
+++ tests/Csv/CsvReaderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sebasbit\LoadData\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CsvReaderTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
