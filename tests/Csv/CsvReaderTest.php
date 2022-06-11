d3ab7200dbe013a1b874b3fba049bf4b78f6715a
sebasbit <sebastian.torres0426@gmail.com>
2022-06-11 18:19:03 -0500

    Refactor CsvReaderTest
    Move path-to-file and expected result to $fixtures property.

--- tests/Csv/CsvReaderTest.php
+++ tests/Csv/CsvReaderTest.php
@@ -8,9 +8,31 @@ use Sebasbit\LoadData\Csv\CsvReaderException;
 
 class CsvReaderTest extends TestCase
 {
+    protected array $fixtures;
+
+    protected function setUp(): void
+    {
+        $this->fixtures = [
+            'empty' => __DIR__ . '/../Fixtures/test-empty.csv',
+            'no-default-options' => [
+                __DIR__ . '/../Fixtures/test-no-default-options.csv',
+                ['1', 'Forrester; Skittle', 'fskittle0@yale.edu']
+            ],
+            'no-headers' => [
+                __DIR__ . '/../Fixtures/test-no-headers.csv',
+                ['1', 'Forrester Skittle', 'fskittle0@yale.edu']
+            ],
+            'with-headers' => [
+                __DIR__ . '/../Fixtures/test-with-headers.csv',
+                ['id', 'name', 'email'],
+                ['1', 'Forrester Skittle', 'fskittle0@yale.edu']
+            ]
+        ];
+    }
+
     public function test_create_with_path_to_file()
     {
-        $file = __DIR__ . '/../Fixtures/test-no-headers.csv';
+        $file = $this->fixtures['empty'];
         $csvReader = new CsvReader($file);
         $this->assertEquals($file, $csvReader->origin());
         $this->assertNull($csvReader->headers());
@@ -21,6 +43,7 @@ class CsvReaderTest extends TestCase
     {
         $this->expectException(CsvReaderException::class);
         $this->expectExceptionMessage("The file 'fail.csv' does not exists");
+
         $csvReader = new CsvReader('fail.csv');
     }
 
@@ -28,13 +51,13 @@ class CsvReaderTest extends TestCase
     {
         $this->expectException(CsvReaderException::class);
         $this->expectExceptionMessage("The file '.' cannot be opened");
+
         $csvReader = new CsvReader('.');
     }
 
     public function test_read_rows_from_the_file()
     {
-        $file = __DIR__ . '/../Fixtures/test-no-headers.csv';
-        $expected = ['1', 'Forrester Skittle', 'fskittle0@yale.edu'];
+        [$file, $expected] = $this->fixtures['no-headers'];
         $csvReader = new CsvReader($file);
 
         $this->assertEquals($expected, $csvReader->nextRow());
@@ -44,50 +67,41 @@ class CsvReaderTest extends TestCase
 
     public function test_return_null_if_the_last_row_was_read()
     {
-        $file = __DIR__ . '/../Fixtures/test-empty.csv';
+        $file = $this->fixtures['empty'];
         $csvReader = new CsvReader($file);
         $this->assertNull($csvReader->nextRow());
     }
 
     public function test_set_options_by_constructor()
     {
-        $file = __DIR__ . '/../Fixtures/test-no-default-options.csv';
-        $expected = ['1', 'Forrester; Skittle', 'fskittle0@yale.edu'];
+        [$file, $expected] = $this->fixtures['no-default-options'];
         $csvReader = new CsvReader($file, ['length' => 41, 'delimiter' => ';', 'enclosure' => '#']);
         $this->assertEquals($expected, $csvReader->nextRow());
     }
 
-    public function test_read_headers_if_option_is_set_to_true()
+    public function test_consider_headers_when_reading_rows()
     {
-        $file = __DIR__ . '/../Fixtures/test-with-headers.csv';
-        $expected = ['id', 'name', 'email'];
+        [$file, $headers, $expected] = $this->fixtures['with-headers'];
         $csvReader = new CsvReader($file, ['headers' => true]);
-        $this->assertEquals($expected, $csvReader->headers());
+
+        $this->assertEquals($headers, $csvReader->headers());
+        $this->assertEquals($expected, $csvReader->nextRow());
+        $csvReader->reset();
+        $this->assertEquals($expected, $csvReader->nextRow());
     }
 
     public function test_fail_if_headers_cannot_be_read()
     {
         $this->expectException(CsvReaderException::class);
         $this->expectExceptionMessage("The headers cannot be read");
-        $file = __DIR__ . '/../Fixtures/test-empty.csv';
-        $csvReader = new CsvReader($file, ['headers' => true]);
-    }
 
-    public function test_consider_headers_when_reading_rows()
-    {
-        $file = __DIR__ . '/../Fixtures/test-with-headers.csv';
-        $expected = ['1', 'Forrester Skittle', 'fskittle0@yale.edu'];
+        $file = $this->fixtures['empty'];
         $csvReader = new CsvReader($file, ['headers' => true]);
-
-        $this->assertEquals($expected, $csvReader->nextRow());
-        $csvReader->reset();
-        $this->assertEquals($expected, $csvReader->nextRow());
     }
 
     public function test_iterate_over_rows_using_a_foreach_loop()
     {
-        $file = __DIR__ . '/../Fixtures/test-no-headers.csv';
-        $expected = ['1', 'Forrester Skittle', 'fskittle0@yale.edu'];
+        [$file, $expected] = $this->fixtures['no-headers'];
         $csvReader = new CsvReader($file);
 
         foreach($csvReader as $rownum => $row) {
